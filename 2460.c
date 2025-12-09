464ab783abad8baf1db9ca692be102f209b3bf0a
Ian Fernandes Matsukuma <ian.matsukuma@gmail.com>
2025-12-09 14:43:17 -0300

    2460.c
    Removed comments about identifier limits and boolean array usage.

---
+++ 2460.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int N, M;
+    scanf("%d", &N);
+
+    int fila[N];
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &fila[i]);
+    }
+
+    scanf("%d", &M);
+
+
+    int saiu[100001] = {0};
+
+    for (int i = 0; i < M; i++) {
+        int id;
+        scanf("%d", &id);
+        saiu[id] = 1;
+    }
+
+  
+    int first = 1;
+    for (int i = 0; i < N; i++) {
+        if (!saiu[fila[i]]) {
+            if (!first) printf(" ");
+            printf("%d", fila[i]);
+            first = 0;
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
